a984afa68f8aaf2c84e6ed17441b46360b9b6c6c
RyanSikandar <ryansikandar@gmail.com>
2024-04-08 17:13:04 +0500

    chnages

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://smart-control-app-frontend.vercel.app"],
     credentials: true
 }));
 app.use(express.urlencoded({ extended: false }));
